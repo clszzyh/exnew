452980a25c2b7bb982ae1366cc90528eacf67b15
clszzyh <clszzyh@gmail.com>
2020-12-24 09:53:23 +0800

    ci
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Exnew
 
-**TODO: Add description**
+<!-- MDOC -->
+
+Elixir struct wrapper.
+
+<!-- MDOC -->
+
 
 ## Installation
 
@@ -18,4 +23,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/exnew](https://hexdocs.pm/exnew).
-
--- mix.exs
+++ mix.exs
@@ -1,13 +1,60 @@
 defmodule Exnew.MixProject do
   use Mix.Project
 
+  @version "VERSION" |> File.read!() |> String.trim()
+  @github_url "https://github.com/clszzyh/exnew"
+  @description "README.md"
+               |> File.read!()
+               |> String.split("<!-- MDOC -->")
+               |> Enum.fetch!(1)
+               |> String.trim()
+
   def project do
     [
       app: :exnew,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
+      elixirc_options: [warnings_as_errors: System.get_env("CI") == "true"],
+      package: [
+        licenses: ["MIT"],
+        files: ["lib", ".formatter.exs", "mix.exs", "README*", "CHANGELOG*", "VERSION"],
+        exclude_patterns: ["priv/plts", ".DS_Store"],
+        links: %{
+          "GitHub" => @github_url,
+          "Changelog" => @github_url <> "/blob/master/CHANGELOG.md"
+        }
+      ],
+      docs: [
+        source_ref: "v" <> @version,
+        source_url: @github_url,
+        main: "readme",
+        extras: ["README.md", "CHANGELOG.md"]
+      ],
+      preferred_cli_env: [ci: :test],
+      dialyzer: [
+        plt_core_path: "priv/plts",
+        plt_add_deps: :transitive,
+        plt_add_apps: [:ex_unit],
+        list_unused_filters: true,
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        flags: dialyzer_flags()
+      ],
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
+    ]
+  end
+
+  defp dialyzer_flags do
+    [
+      :error_handling,
+      :race_conditions,
+      # :underspecs,
+      :unknown,
+      :unmatched_returns
+      # :overspecs
+      # :specdiffs
     ]
   end
 
@@ -21,8 +68,22 @@ defmodule Exnew.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      ci: [
+        "compile --warnings-as-errors --force --verbose",
+        "format --check-formatted",
+        "credo --strict",
+        "docs",
+        "dialyzer",
+        "test"
+      ]
     ]
   end
 end
